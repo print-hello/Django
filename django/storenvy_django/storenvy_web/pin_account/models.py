3b03c6a2eb551adf9409fb547a091510ea78ceca
printhello <printhello@163.com>
2019-01-29 17:23:08 +0800

    添加配置表显示

--- django/storenvy_django/storenvy_web/pin_account/models.py
+++ django/storenvy_django/storenvy_web/pin_account/models.py
@@ -1,12 +1,4 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
-
 from django.db import models
 
 
@@ -37,26 +29,6 @@ class Account(models.Model):
         db_table = 'account'
 
 
-class AccountCount(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    all_count = models.IntegerField(blank=True, null=True)
-    real_time_num = models.IntegerField(blank=True, null=True)
-    last_update_time = models.DateField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'account_count'
-
-
-class BoardTemplate(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    board_name = models.CharField(max_length=255, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'board_template'
-
-
 class Configuration(models.Model):
     id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
     random_browsing_control = models.IntegerField()
@@ -64,98 +36,19 @@ class Configuration(models.Model):
     browsing_pic_max_num = models.IntegerField()
     access_home_page_control = models.IntegerField()
     save_pic_control = models.IntegerField()
+    upload_pic_control = models.IntegerField()
+    upload_pic_min_num = models.IntegerField()
+    upload_pic_max_num = models.IntegerField()
     follow_num = models.IntegerField()
     pin_self_count = models.IntegerField()
     scroll_num = models.IntegerField()
     create_board_num = models.IntegerField()
     search_words_count = models.IntegerField()
+    save_pic_from_homepage_control = models.IntegerField()
+    click_specific_pin_control = models.IntegerField()
     priority = models.IntegerField()
     content = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'configuration'
-
-
-class FollowUrl(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    web_url = models.CharField(max_length=255, blank=True, null=True)
-    home_url = models.CharField(max_length=255, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'follow_url'
-
-
-class Ips(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    ip = models.CharField(max_length=20, blank=True, null=True)
-    a_segment = models.IntegerField(blank=True, null=True)
-    b_segment = models.IntegerField(blank=True, null=True)
-    c_segment = models.IntegerField(blank=True, null=True)
-    used = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'ips'
-
-
-class OtherPinHistory(models.Model):
-    account_id = models.IntegerField()
-    pin_url = models.CharField(max_length=200)
-    pin_pic_url = models.CharField(max_length=255, blank=True, null=True)
-    add_time = models.DateTimeField()
-    hostname = models.CharField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'other_pin_history'
-
-
-class PinHistory(models.Model):
-    account_id = models.IntegerField()
-    pin_url = models.CharField(max_length=200)
-    pin_pic_url = models.CharField(max_length=255, blank=True, null=True)
-    add_time = models.DateTimeField()
-    hostname = models.CharField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'pin_history'
-
-
-class PortInfo(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    port = models.IntegerField(blank=True, null=True)
-    ip = models.CharField(max_length=255, blank=True, null=True)
-    password = models.CharField(max_length=255, blank=True, null=True)
-    zone = models.CharField(max_length=255, blank=True, null=True)
-    state = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'port_info'
-
-
-class SearchWords(models.Model):
-    word = models.CharField(max_length=100)
-    state = models.IntegerField()
-    boards = models.CharField(max_length=100)
-    us = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'search_words'
-
-
-class UserAgent(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    user_agent = models.CharField(max_length=255, blank=True, null=True)
-    system = models.CharField(max_length=255, blank=True, null=True)
-    terminal = models.CharField(max_length=255, blank=True, null=True)
-    read_time = models.IntegerField(blank=True, null=True)
-    state = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'user_agent'
--- django/storenvy_django/storenvy_web/pin_account/views.py
+++ django/storenvy_django/storenvy_web/pin_account/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import *
+import datetime
 
 
 def pin_views(request):
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d")
     s = Account.objects.using('pin_db').filter(port__gte=100)
     all_count = Account.objects.using('pin_db').filter(port__gte=100).count()
     right_count = Account.objects.using('pin_db').filter(state=1).filter(port__gte=100).count()
     ip_count = Account.objects.using('pin_db').filter(state=2).filter(port__gte=100).count()
-
+    run_today = Account.objects.using('pin_db').filter(action_time__gte=current_time).count()
+    config = Configuration.objects.using('pin_db')
     return render(request, 'pin.html', locals())
